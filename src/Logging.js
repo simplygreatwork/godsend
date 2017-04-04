a9dedf7d1daf68cdb5cf3c97476add42f96e5224
Philip Weaver <simplygreatwork@gmail.com>
2017-04-04 04:49:23 +0000

    Improving logging support.

--- src/Logging.js
+++ src/Logging.js
@@ -2,12 +2,12 @@ var Logger = require('js-logger');
 var Bus = require('./Bus');
 
 module.exports = Class.extend({
-
+	
 	initialize: function(properties) {
-
+		
 		Object.assign(this, properties);
 		this.initializeLogger();
-		this.initializeHandlers();
+		if (false) this.initializeHandlers();
 		this.remotes = [];
 	},
 	
@@ -15,17 +15,23 @@ module.exports = Class.extend({
 		
 		Logger.useDefaults();
 		Logger.setLevel(Logger.INFO);
-		Logger.get('broker').setLevel(Logger.INFO);
-		Logger.get('exchange').setLevel(Logger.INFO);
-		Logger.get('bus').setLevel(Logger.INFO);
-		Logger.get('cache').setLevel(Logger.INFO);
-		Logger.get('connection').setLevel(Logger.INFO);
-		Logger.get('register').setLevel(Logger.INFO);
-		Logger.get('request').setLevel(Logger.INFO);
-		Logger.get('transport').setLevel(Logger.INFO);
-		Logger.get('server').setLevel(Logger.INFO);
-		Logger.get('server-web').setLevel(Logger.INFO);
-		Logger.get('server-socket').setLevel(Logger.INFO);
+		this.initializeInternalLevels(Logger.INFO);
+		this.initializeInternalLevels(Logger.OFF);
+	},
+	
+	initializeInternalLevels : function(level) {
+		
+		Logger.get('broker').setLevel(level);
+		Logger.get('exchange').setLevel(level);
+		Logger.get('bus').setLevel(level);
+		Logger.get('cache').setLevel(level);
+		Logger.get('connection').setLevel(level);
+		Logger.get('register').setLevel(level);
+		Logger.get('request').setLevel(level);
+		Logger.get('transport').setLevel(level);
+		Logger.get('server').setLevel(level);
+		Logger.get('server-web').setLevel(level);
+		Logger.get('server-socket').setLevel(level);
 	},
 	
 	initializeHandlers: function() {
@@ -59,7 +65,7 @@ module.exports = Class.extend({
 			}.bind(this)
 		});
 	},
-
+	
 	push: function(messages, context) { // need to restrict named log set : else infinite recursion from exchange logs
 		// however developers won't typically have access to the server Logger instance
 		if (this.connection && this.remotes.indexOf(context.name) > -1) {
